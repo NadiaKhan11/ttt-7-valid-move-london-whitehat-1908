f4b1f91a5516dfdeae9f276b5ec24c2a678c2fe9
Nadia Khan <nadia.khan@sky.uk>
2019-08-08 08:40:56 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,10 @@
 def valid_move?(board, index)
-   if index.between?(0,8) && !position_taken?(board, index)
+   if index.between?(0,8) && !position_taken?(board, index) then
      return true
-end
+   else
+     return false
+   end
+end 
 
 def position_taken?(borad, index)
   
